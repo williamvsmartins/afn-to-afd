caa9d80b98cfff984ca2d077aa0fa3e92a5f8e3e
williamvsmartins <williamvaltherprogramador@gmail.com>
2025-01-16 11:36:31 -0300

    feat: add function to compute epsilon closure for automata

--- automata.py
+++ automata.py
@@ -6,3 +6,18 @@ def adicionarTransicao( automato, origem, letra, destino ):
         automato[ "função_programa" ][ origem ][letra] = set()
     
     automato[ "função_programa" ][ origem ][letra].add( destino )
+
+def fecho_vazio( automato, estados ):
+    fecho = set( estados )
+    fila = list( estados )
+
+    while fila:
+        estado = fila.pop( 0 )
+        if estado in automato[ "função_programa" ] and "" in automato[ "função_programa" ][ estado ]:
+            estados_novos = automato[ "função_programa" ][ estado ][ "" ]
+            for estado_novo in estados_novos:
+                if estado_novo not in fecho:
+                    fecho.add( estado_novo )
+                    fila.append( estado_novo )
+    
+    return fecho
