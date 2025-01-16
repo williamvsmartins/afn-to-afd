93220841ac1264bcc0b74bed7eacf764eb3d322f
williamvsmartins <williamvaltherprogramador@gmail.com>
2025-01-16 11:37:07 -0300

    feat: add function to convert NFA to DFA

--- automata.py
+++ automata.py
@@ -21,3 +21,50 @@ def fecho_vazio( automato, estados ):
                     fila.append( estado_novo )
     
     return fecho
+
+def afn_para_afd( automato ):
+    afd = {
+        "alfabeto" : set(),
+        "estados" : set(),
+        "função_programa" : dict(),
+        "estado_inicial" : "0",
+        "estados_finais" : set(),
+    }
+
+    afd[ "alfabeto" ].add( "$" )
+
+    estado_inicial = ",".join( sorted( fecho_vazio( automato, { "0" } ) ) )
+
+    afd[ "estados" ].add( estado_inicial )
+
+    fila = [ estado_inicial ]
+    visitados = set()
+
+    while fila:
+        estado_atual = fila.pop( 0 )
+        if estado_atual in visitados:
+            continue
+        
+        visitados.add( estado_atual )
+        estados_atuais = estado_atual.split( "," )
+
+        for letra in automato[ "alfabeto" ]:
+            novos_estados = set()
+            for estado in estados_atuais:
+                if estado in automato[ "função_programa" ] \
+                    and letra in automato[ "função_programa" ][ estado ]:
+                    novos_estados = automato[ "função_programa" ][ estado ][ letra ]
+            
+            if novos_estados:
+                novo_estado = ",".join( sorted( novos_estados ) )
+                if novo_estado not in afd[ "estados" ]:
+                    fila.append( novo_estado )
+                    afd[ "estados" ].add( novo_estado )
+                    estados = [ estado in automato[ "estados_finais" ] for estado in novos_estados ]
+                    if any( estados ):
+                        afd[ "estados_finais" ].add( novo_estado )
+                adicionarTransicao( afd, estado_atual, letra, novo_estado )
+            else:
+                adicionarTransicao( afd, estado_atual, letra, "" )
+    
+    return afd
